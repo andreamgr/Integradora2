a6825d390b568af0ef8db6df0f1a3e321dda57dc
Alan Cruz <alan96942@gmail.com>
2021-10-07 12:25:44 -0500

    primer commit

---
+++ nbproject/Makefile-variables.mk
@@ -0,0 +1,13 @@
+#
+# Generated - do not edit!
+#
+# NOCDDL
+#
+CND_BASEDIR=`pwd`
+# default configuration
+CND_ARTIFACT_DIR_default=dist/default/production
+CND_ARTIFACT_NAME_default=Practica_Integradora2.X.production.hex
+CND_ARTIFACT_PATH_default=dist/default/production/Practica_Integradora2.X.production.hex
+CND_PACKAGE_DIR_default=${CND_DISTDIR}/default/package
+CND_PACKAGE_NAME_default=practicaintegradora2.x.tar
+CND_PACKAGE_PATH_default=${CND_DISTDIR}/default/package/practicaintegradora2.x.tar
